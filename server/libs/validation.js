1e10ec4b695dce364343fc003b9a268ff128828b
Trygve Sørensen <trygvsor@stud.ntnu.no>
2017-02-03 16:32:53 +0100

    Survey validation (should) now work
    Full validation should work, but should also be refactored

--- server/libs/validation.js
+++ server/libs/validation.js
@@ -38,6 +38,32 @@ exports.surveyValidation = function(receivedSurvey) {
     stringVal(receivedSurvey.name) &&
     objectVal(receivedSurvey.questionlist);
 
+  // further check the questionlist for validity
+  if (valid) {
+    // for each question
+    for (let i = 0; i < receivedSurvey.questionlist; i++) {
+      // make sure there exists a mode
+      valid = valid && stringVal(receivedSurvey.questionlist[i].mode);
+      if (valid) {
+        // for each language option, we check these independently.
+        for (let lang in ['eng', 'nor']) {
+          let langIsValid = objectVal(receivedSurvey.questionlist[i][lang])
+            && receivedSurvey.questionlist[i][lang].options;
+          if (langIsValid) {
+            // if lang is valid, we do checks for the question text and the alternatives for said question
+            langIsValid = langIsValid && stringVal(receivedSurvey.questionlist[i][lang].txt);
+            // for each alternative to the question
+            for (let j = 0; j < receivedSurvey.questionlist[i][lang].options; j++) {
+              langIsValid = langIsValid && stringVal(receivedSurvey.questionlist[i][lang].options[j]);
+            }
+            // still within the if check, we make sure that IF AND ONLY IF the
+            // langauge was valid, we change up the validity of the whole survey.
+            valid = valid && langIsValid;
+          }
+        }
+      }
+    }
 
-  return valid;
+    return valid;
+  }
 };
