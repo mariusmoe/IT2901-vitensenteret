0fb9acbb5f59df3b4524c6b099c0b34d525fea93
birathek <birathet@stud.ntnu.no>
2017-02-24 12:39:32 +0100

    Component fills the  functional requirements needed

--- client_viten/src/app/user/choose-survey/choose-survey.component.ts
+++ client_viten/src/app/user/choose-survey/choose-survey.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,ElementRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, OnDestroy } from '@angular/core';
 import { SurveyList } from '../../_models/survey_list';
 import { SurveyService } from '../../_services/survey.service';
 
@@ -8,74 +8,95 @@ import { SurveyService } from '../../_services/survey.service';
   templateUrl: './choose-survey.component.html',
   styleUrls: ['./choose-survey.component.scss']
 })
-export class ChooseSurveyComponent implements OnInit,OnDestroy {
+export class ChooseSurveyComponent implements OnInit, OnDestroy {
 
   private allsurveys: SurveyList[];
   private searchedsurveys: SurveyList[];
   private recentsurveys;
+  private recentsurveysmax;
 
 
-  private loaded: boolean = false;
-  private search:string="";
-  private search_result: boolean= false;
+  private loaded = false;
+  private search = '';
+  private search_result = false;
+  private tall = 0;
 
 
 
 
-  constructor(private surveyService: SurveyService){
+  constructor(private surveyService: SurveyService) {
     this.allsurveys = [];
-    this.recentsurveys = [];
     this.searchedsurveys = [];
+    this.recentsurveys = [];
+    this.recentsurveysmax = [];
   }
 
   ngOnInit() {
+    // Loads all surveys from the local database to list
     this.surveyService.getAllSurveys().subscribe( (surveys) => {
-      for (let survey of surveys) {
+      for (const survey of surveys) {
         if (!survey.active) { continue; }
         this.allsurveys.push(survey);
       }
-      console.log(this.allsurveys);
+
+      // No use of this yet since the database keeps the recent survey in the last indext of all surveys
+      /*
+      for (const survey of this.allsurveys) {
+        this.recentsurveys.push([this.formatmilliseconds(survey.date), survey._id, survey.name]);
+      }
+      this.recentsurveys.reverse();
+       */
+      this.allsurveys.reverse();
+
+      // Shows the 10 last surveys of all surveys
+      for (const limit of this.allsurveys){
+          if (this.tall === 10) {
+            break;
+          }else {
+            this.recentsurveysmax.push(limit);
+            this.tall++;
+          };
+      }
+
       this.loaded = true;
     });
 
-    this.recent();
+
+
+}
+
+  ngOnDestroy() {
   }
 
-  ngOnDestroy(){
-  }
-
-clicked(name,_id):void{
-  for (let survey of this.allsurveys){
-      if(survey._id.toString() == _id.toString()){
-        console.log("the id exists");
+clicked(name, _id): void {
+  for (const survey of this.allsurveys){
+      if (survey._id.toString() === _id.toString()) {
+        console.log('the id exists');
     }
   }
 }
 
 searched(search): void {
-  if(this.search_result == false){
+  if (this.search_result === false) {
     this.search_result = !this.search_result;
   }
   this.searchedsurveys.splice(0);
-  for(let survey of this.allsurveys){
-    if(survey.name.toLowerCase().indexOf(search.toLowerCase().trim()) > -1){
+  for (const survey of this.allsurveys){
+    if (survey.name.toLowerCase().indexOf(search.toLowerCase().trim()) > -1) {
       this.searchedsurveys.push(survey);
         }
   }
-  console.log("results"+this.searchedsurveys)
+  console.log('results' + this.searchedsurveys);
 }
 
 
-recent(): void{
-for(let survey of this.allsurveys){
-  this.recentsurveys.push(this.formatmilliseconds(survey.date));
+recent(): void {
 
-  console.log(this.allsurveys);
-  console.log(this.recentsurveys);
+    }
 
-  }
 
-}
-formatDate(date): string{  return new Date(date).toLocaleDateString();}
-formatmilliseconds(date): number{  return new Date(date).valueOf();}
+
+
+formatDate(date): string {  return new Date(date).toLocaleDateString(); }
+formatmilliseconds(date): number {  return new Date(date).valueOf(); }
 }
