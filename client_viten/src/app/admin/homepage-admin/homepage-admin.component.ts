f123d90a0b1f4088052b216e4b6c3e7da8024c8e
Trygve Sørensen <trygvsor@stud.ntnu.no>
2017-02-25 17:16:58 +0100

    Fix: selected route bug from last commit

--- client_viten/src/app/admin/homepage-admin/homepage-admin.component.ts
+++ client_viten/src/app/admin/homepage-admin/homepage-admin.component.ts
@@ -1,36 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SurveyService } from '../../_services/survey.service';
 import { Survey } from '../../_models/survey';
-import { Router, ActivatedRoute, Params, NavigationStart } from '@angular/router';
-
+import { Router, ActivatedRoute, Params, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-homepage-admin',
   templateUrl: './homepage-admin.component.html',
   styleUrls: ['./homepage-admin.component.scss']
 })
-export class HomepageAdminComponent implements OnInit {
+export class HomepageAdminComponent implements OnInit, OnDestroy {
   survey: Survey = null;
   loadingSurvey = false;
 
+  private routerSub: Subscription;
+
   constructor(private surveyService: SurveyService, private router: Router, private route: ActivatedRoute) {
-    // set survey to null initially.
-    this.survey = null;
-    // If we have a router parameter, we should attempt to use that first.
-    const param = this.route.snapshot.params['surveyId'];
-    if (param) {
-      this.getSurvey(param);
-    }
-    // whenever the route *starts* to change we should be up and ready to query for data
-    // so that it is available to the user asap.
-    this.router.events.filter(event => event instanceof NavigationStart).subscribe( (event: NavigationStart) => {
-      const newParam: string = event.url.slice(event.url.lastIndexOf('/') + 1); // + 1 to remove the /
-      this.getSurvey(newParam);
-    });
   }
 
   ngOnInit() {
+    // whenever we navigate, we should query for the survey.
+    this.routerSub = this.router.events.filter(event => event instanceof NavigationEnd).subscribe( (event: NavigationEnd) => {
+      const param = this.route.snapshot.params['surveyId'];
+      if (param) {
+        this.getSurvey(param);
+      }
+    });
+  }
 
+  ngOnDestroy() {
+    // unsubscribe when this component gets destroyed!
+    this.routerSub.unsubscribe();
   }
 
   private getSurvey(surveyId: string) {
