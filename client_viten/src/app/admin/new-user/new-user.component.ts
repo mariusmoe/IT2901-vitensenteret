747f72d83d57114c4760ef5731672236915a6e42
seroc000 <sondrek@getmail.no>
2017-02-28 15:35:58 +0100

    Created the component for adding a new user. The component has valid form control, but the answers are not processed yet.

--- client_viten/src/app/admin/new-user/new-user.component.ts
+++ client_viten/src/app/admin/new-user/new-user.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { slideInDownAnimation } from '../../animations';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../_services/authentication.service';
+import { User } from '../../_models/user';
 
 @Component({
   selector: 'app-new-user',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  newUserForm: FormGroup;
+  loading = false;
+  error: string;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private authenticationService: AuthenticationService) {
+      this.newUserForm = fb.group({
+      'email': [null, Validators.required],
+      'password': [null, Validators.required],
+      'passwordconfirm': [null, Validators.required]
+    });
+   }
 
   ngOnInit() {
+  }
+
+  submitForm(form: {email: string, password: string, passwordconfirm: string}): void {
+    this.loading = true;
+    console.log(form);
+
+    if (form.password === form.passwordconfirm) {
+      console.log('success');
+    }
+    else{
+      console.log('failure');
+    }
+
   }
 
 }
