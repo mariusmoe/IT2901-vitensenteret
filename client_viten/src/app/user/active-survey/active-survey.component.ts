8926e3c4e6ff56886f9597304315a242894f1048
Christian <christrh@stud.ntnu.no>
2017-02-28 14:50:57 +0100

    Working animations have been added between questions

--- client_viten/src/app/user/active-survey/active-survey.component.ts
+++ client_viten/src/app/user/active-survey/active-survey.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, transition, style, keyframes, animate } from '@angular/core';
 import { SurveyService } from '../../_services/survey.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Survey, QuestionObject } from '../../_models/survey';
@@ -7,14 +7,24 @@ import { Survey, QuestionObject } from '../../_models/survey';
 @Component({
   selector: 'active-survey',
   templateUrl: './active-survey.component.html',
-  styleUrls: ['./active-survey.component.scss']
+  styleUrls: ['./active-survey.component.scss'],
+  animations: [
+    trigger('easeInOut', [
+      state('void', style({'opacity': 0})),
+      transition(':enter', animate('0.5s ease-in-out', style({'opacity': 1}))),
+      transition(':leave', animate('0.5s ease-in-out', style({'opacity': 0})))
+    ])
+  ]
 })
+
 export class ActiveSurveyComponent implements OnInit {
-  private properSurvey: boolean = false;
-  private started: boolean = false;
+  private properSurvey = false;
+  private started = false;
   private survey: Survey;
-  private page: number = 0;
-  private totalPages: number = 0;
+  private page = 0;
+  private totalPages = 0;
+  private nextPage = 0;
+  private transition = false;
 
   constructor(private surveyService: SurveyService,
     private router: Router, private route: ActivatedRoute) {
@@ -26,7 +36,7 @@ export class ActiveSurveyComponent implements OnInit {
     if (this.route.snapshot.params['surveyId']){
       this.surveyService.getSurvey(this.route.snapshot.params['surveyId']).subscribe(result => {
         if (!result) {
-          console.log("DEBUG: BAD surveyId param from router!");
+          // console.log("DEBUG: BAD surveyId param from router!");
           // TODO: Redirect to base create survey ?
           return;
         }
@@ -34,7 +44,7 @@ export class ActiveSurveyComponent implements OnInit {
         this.totalPages = this.survey.questionlist.length;
 
         if (this.survey && this.survey.active) {
-          console.log(this.survey);
+          // console.log(this.survey);
           this.properSurvey = true;
         }
       });
@@ -52,16 +62,30 @@ export class ActiveSurveyComponent implements OnInit {
         return;
       }
       this.page -= 1;
+      this.transition = true;
+
       // console.log('previous question');
     }
 // When you navigate to the next question, this happens
   private nextQ(){
     if(this.page+1 >= this.totalPages){
       // console.log("this is the last question, can't advance further");
+
+      //TODO transition to endMessage
       return;
     }
     this.page += 1;
+    this.transition = true;
     // console.log('next question');
+  }
+
+  animEnd(event) {
+    if (!event.fromState) {
+      this.transition = false;
+      // console.log("Going to next page now!")
+    }
+    // console.log('animEnd');
+    // console.log(event);
   }
 
 }
