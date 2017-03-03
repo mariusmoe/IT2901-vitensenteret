20d7b45fc7270e1ca76231eae2311f65b1065d82
Christian <christrh@stud.ntnu.no>
2017-03-03 15:28:43 +0100

    Stars DONE + minor adjust on Smiley

--- client_viten/src/app/user/smiley/smiley.component.ts
+++ client_viten/src/app/user/smiley/smiley.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { QuestionObject } from '../../_models/survey';
 
-
 @Component({
   selector: 'app-smiley',
   templateUrl: './smiley.component.html',
@@ -10,14 +9,21 @@ import { QuestionObject } from '../../_models/survey';
 export class SmileyComponent implements OnInit {
   @Input() questionObject: QuestionObject;
   @Output() answer = new EventEmitter();
+  selectedSmile: number;
 
   constructor() {  }
 
   ngOnInit() {}
 
 // This method emits the changes to its parent. The parent HTML listens for $event changes and call the addOrChangeAnswer(alt)
-  addChange(alt){
+  addChange(alt) {
     console.log('Answer changed');
     this.answer.emit(alt);
+  }
+
+  // This method selects the smiley and calls addChange()
+  selectSmile(selectedSmile) {
+    this.selectedSmile = selectedSmile;
+    this.addChange(this.selectedSmile);
   }
 }
