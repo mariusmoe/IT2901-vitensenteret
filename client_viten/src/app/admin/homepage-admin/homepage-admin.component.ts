b8eedd05344c3f37aa7f3afc1c5aa1338a1ab48c
Trygve Sørensen <trygvsor@stud.ntnu.no>
2017-03-04 01:34:34 +0100

    Fix: Bug narrow graph display issue in PDFs, Update: Add more text to the PDF

--- client_viten/src/app/admin/homepage-admin/homepage-admin.component.ts
+++ client_viten/src/app/admin/homepage-admin/homepage-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, ViewChild, animate, state, style, transition, trigger  } from '@angular/core';
 import { SurveyService } from '../../_services/survey.service';
+import { TranslateService } from '../../_services/translate.service';
 import { Survey } from '../../_models/survey';
 import { Router, ActivatedRoute, Params, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
@@ -30,7 +31,8 @@ export class HomepageAdminComponent implements OnInit, OnDestroy {
   constructor(private surveyService: SurveyService,
     private router: Router,
     private route: ActivatedRoute,
-    private datePipe: DatePipe) {
+    private datePipe: DatePipe,
+    private translateService: TranslateService) {
   }
 
   ngOnInit() {
@@ -48,6 +50,10 @@ export class HomepageAdminComponent implements OnInit, OnDestroy {
     this.routerSub.unsubscribe();
   }
 
+  /**
+   * Loads a survey into the component
+   * @param  {string} surveyId the id of the survey to load
+   */
   private getSurvey(surveyId: string) {
     this.loadingSurvey = true;
     this.surveyService.getSurvey(surveyId).subscribe( (survey: Survey) => {
@@ -80,63 +86,77 @@ export class HomepageAdminComponent implements OnInit, OnDestroy {
     );
   }
 
+  /**
+   * Initiates a download of a PDF containing data of the given survey
+   */
   downloadAsPDF() {
-    const that = this;
+    // notify component that we are generating the PDF
     this.generatingPDF = true;
     const pdf = new jsPDF();
-    const filename = this.survey.name.replace(/ /g, '_').replace(/\?/g, '').replace(/\./g, '') + '.pdf';
 
-    pdf.setFontSize(20);
-    pdf.text(25, 20, this.survey.name);
-    pdf.setFontSize(12);
-    pdf.text(25, 30, this.survey.comment);
-    pdf.text(25, 40, 'Date created: ' + this.datePipe.transform(this.survey.date, 'yyyy-MM-dd'));
+    // define functions that allow us some text position manipulation
+    const centeredText = function(y, text) {
+      const textWidth = pdf.getStringUnitWidth(text) * pdf.internal.getFontSize() / pdf.internal.scaleFactor;
+      const textOffset = (pdf.internal.pageSize.width - textWidth) / 2;
+      pdf.text(textOffset, y, text);
+    };
+    const rightAlignedText = function(margin, y, text) {
+      const textWidth = pdf.getStringUnitWidth(text) * pdf.internal.getFontSize() / pdf.internal.scaleFactor;
+      const textOffset = pdf.internal.pageSize.width - textWidth - margin;
+      pdf.text(textOffset, y, text);
+    };
+
+    // start wroting data to the PDF.
+    pdf.setFontSize(30);
+    centeredText(22, 'Vitensenteret');
+    pdf.setFontSize(18);
+    pdf.text(25, 35, this.survey.name);
+    pdf.setFontSize(8);
+    pdf.text(25, 44, this.survey.comment);
+    // Dates and num answers are right-aligned
+    rightAlignedText(25, 34, this.translateService.instant('Date created: d', this.datePipe.transform(this.survey.date, 'yyyy-MM-dd')));
     const now = Date.now();
     const nowText = this.datePipe.transform(now, 'yyyy-MM-dd');
-    pdf.text(25, 45, 'Date printed: ' + nowText);
-    const canvases = this.surveyDOM.nativeElement.querySelectorAll('canvas');
-    console.log(canvases);
-    const images = [];
+
+    rightAlignedText(25, 39, this.translateService.instant('Date printed: d', nowText));
+    const answers = this.survey.questionlist[0].answer.length || 0;
+    rightAlignedText(25, 44, this.translateService.instant('Number of responses: n', answers.toString()));
+
+    // Get our charts (canvases)
+    const canvases = this.surveyDOM.nativeElement.querySelectorAll('canvas.surveyQuestionChart');
+    // Set up a dummy canvas. This dummy canvas is used to set a white background
+    // and to avoid other corruptions to / of the actual real canvases
+    const dummyCanvas = document.createElement('canvas');
+    const dummyCanvasContext = dummyCanvas.getContext('2d');
+    dummyCanvas.width = canvases[0].height * 2;
+    dummyCanvas.height = canvases[0].height;
+    dummyCanvasContext.fillStyle = '#FFFFFF';
+
+    // Declare variables that are used to handle positioning of our charts
     let index = 0;
     let i = 0;
     let firstPage = 35;
     for (const canvas of canvases) {
+      // Add another page if necessary
       if ((i * 90) > 180 || index === 2) {
         pdf.addPage();
         i = 0;
       }
-      pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 25, 20 + firstPage + (i * 90), 160, 80);
-      // pdf.text(10, 40 + i * 90, 'this.survey.name');
+      // Draw our white background on the dummy canvas
+      dummyCanvasContext.fillRect(0, 0, dummyCanvas.width, dummyCanvas.height);
+      // Draw the chart from the real canvas onto our dummy canvas, centered in the dummy canvas
+      dummyCanvasContext.drawImage(canvas, dummyCanvas.width / 2 - canvas.width / 2, 0);
+      // Add the chart with white background into our PDF at the right position
+      pdf.addImage(dummyCanvas.toDataURL('image/jpeg', 0.6), 'JPEG', 25, 20 + firstPage + (i * 90), 160, 80);
+      // Update our positioning variables
       if (index === 1) { firstPage = 0; }
       i++;
       index++;
     }
-
-    // console.log(images);
-    // pdf.addImage(imgData,'JPEG',15,40,180,160)
+    // save our doc with a OS-friendly filename that is related to the survey at hand
+    const filename = this.survey.name.replace(/ /g, '_').replace(/\?/g, '').replace(/\./g, '') + '.pdf';
     pdf.save(filename);
+    // update our component with our new status
     this.generatingPDF = false;
-    //  pdf.createPdf(docDefinition).download('optionalName.pdf');
-
-  //
-  //   function canvasesAdded() {
-  //     pdf.save(filename);
-  //     that.generatingPDF = false;
-  //   }
-  //
-  //
-  //   function recursiveAddQuestions(index, offset) {
-  //     if (index < canvases.length) {
-  //       pdf.addHTML(canvases[index], 0, offset, function() {
-  //         recursiveAddQuestions(index + 1, offset + 100);
-  //         console.log('added following canvas at offset: ' + 100);
-  //         console.log(canvases[index]);
-  //       });
-  //     } else {
-  //       canvasesAdded();
-  //     }
-  //   }
-  //   // add canvases
-  //   recursiveAddQuestions(0, 30);
   }
 }
