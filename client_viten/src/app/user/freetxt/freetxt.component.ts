afd72e266afc7300772d5e325ccb60cfd0f941f8
Christian <christrh@stud.ntnu.no>
2017-03-21 13:15:58 +0100

    Add: functioning freetxt component. Changed stylings for tablets

--- client_viten/src/app/user/freetxt/freetxt.component.ts
+++ client_viten/src/app/user/freetxt/freetxt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { QuestionObject } from '../../_models/survey';
 
 @Component({
@@ -8,10 +8,32 @@ import { QuestionObject } from '../../_models/survey';
 })
 export class FreetxtComponent implements OnInit {
   @Input() questionObject: QuestionObject;
-  
+  @Output() answer = new EventEmitter();
+  public textValue = '';
+  option;
+  writtenAnswer;
+
   constructor() { }
 
   ngOnInit() {
+    this.option = this.questionObject.lang.no.txt;
+  }
+
+  /**
+   * This method emits the changes to its parent. The parent HTML listens for $event changes and call the addOrChangeAnswer(alt)
+   * @param  {number[]} alt The output answer sent to active-survey-component
+   */
+  addChange(alt) {
+    this.answer.emit(alt);
+  }
+
+/**
+ * This updates the variables that posts to the active-survey component
+ * @param  {number[]} userChoice An output number that shows which answer was chosen by a user
+ */
+  updateAnswers() {
+    this.writtenAnswer = this.textValue;
+    this.answer.emit(this.writtenAnswer);
   }
 
 }
