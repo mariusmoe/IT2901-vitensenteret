22ef1a771390fda2c6cfbd7958048868c4746a64
Trygve Sørensen <trygvsor@stud.ntnu.no>
2017-03-29 12:45:33 +0200

    Fix2: ecosystem files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // First application
     {
       name      : "API",
-      script    : "server/index.js",
+      script    : "index.js",
       env: {
         COMMON_VARIABLE: "true"
       },
