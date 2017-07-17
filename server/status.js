80a748d07a675a175e031cdd4cd353d43756e0b1
Trygve Sørensen <trygvsor@stud.ntnu.no>
2017-07-17 14:12:12 +0200

    Add: Contextual error messages to referral links

--- server/status.js
+++ server/status.js
@@ -28,13 +28,17 @@ module.exports = {
     message: 'Referral link established correctly',
     code: 1012
   },
-  NO_REFERRAL_LINK: {
-    message: 'You need a referral link',
+  REFERRAL_LINK_EXPIRED: {
+    message: 'The referral link has expired.',
     code: 2016
   },
-  NOT_AN_ACTIVE_REFERRAL: {
-    message: 'The referral link is too old or not correct',
+  REFERRAL_LINK_WRONG: {
+    message: 'The referral link provided is wrong',
     code: 2017
+  },
+  REFERRAL_LINK_USED: {
+    message: 'The referral link provided has already been used',
+    code: 2018
   },
   ROLE_INCORRECT: {
     message: 'The role provided is invalid',
@@ -42,7 +46,7 @@ module.exports = {
   },
   ROUTE_USERS_VALID_NO_USERS: {
     message: 'Route valid but no users was found',
-    code: 2024
+    code: 2031
   },
   EMAIL_CHANGED: {
     message: 'Email changed',
@@ -57,15 +61,15 @@ module.exports = {
   // SURVEY CODES
   SURVEY_UNPROCESSABLE: {
     message: 'Survey could not be processed. Check validity.',
-    code: 2018
+    code: 2019
   },
   SURVEY_NOT_FOUND: {
     message: 'Survey could not be found',
-    code: 2019
+    code: 2020
   },
   SURVEY_OBJECT_MISSING: { // used when a survey is supposedly being sent TO the server
     message: 'Could not find a survey',
-    code: 2020
+    code: 2021
   },
   SURVEY_UPDATED: {
     message: 'Survey has been updated',
@@ -77,41 +81,41 @@ module.exports = {
   },
   SURVEY_BAD_ID: {
     message: 'The id provided for the survey is malformed',
-    code: 2021
+    code: 2022
   },
   SURVEY_COPY_FAILED: {
     message: 'The copy failed at copying the survey',
-    code: 2022
+    code: 2023
   },
   SURVEY_COPY_FAILED_RESPONSES: {
     message: 'The copy failed at copying responses',
-    code: 2023
+    code: 2024
   },
   SURVEY_DELETED: {
     message: 'Survey has been deleted',
-    code: 2026
+    code: 2025
   },
   NO_NICKNAME_PROVIDED: {
     message: 'No nickname provided',
-    code: 2027
+    code: 2026
   },
   NICKNAME_TAKEN: {
     message: 'nickname taken',
-    code: 2028
+    code: 2027
   },
   UNKNOWN_NICKNAME: {
     message: 'Unknown nickname',
-    code: 2029
+    code: 2028
   },
   NO_NICKNAMES_FOUND: {
     message: 'Could not find any nicknames',
-    code: 2030
+    code: 2029
   },
 
   // ROUTER CODES
   ROUTE_INVALID: {
     message: "The requested route does not exist. Did you forget a param?",
-    code: 2025
+    code: 2030
   },
   ROUTE_SURVEYS_VALID_NO_SURVEYS: {
     message: "Request successful, but no surveys exist.",
@@ -125,10 +129,10 @@ module.exports = {
   },
   SURVEY_RESPONSE_UNPROCESSABLE: {
     message: 'Response could not be processed. Check validity.',
-    code: 2023
+    code: 2031
   },
   SURVEY_RESPONSE_OBJECT_MISSING: {
     message: 'Could not find a response.',
-    code: 2024
+    code: 2032
   }
 }
