7ee913205e34255889263557c877ece36ed0c22b
Unknown <mariusomoe@gmail.com>
2017-07-28 15:04:03 +0200

    fix proxy urls

--- client_viten/src/environments/environment.prod.ts
+++ client_viten/src/environments/environment.prod.ts
@@ -20,19 +20,19 @@ export const environment = {
 
     URL: {
 
-      login: 'https://survey.vitensenteret.com:2000/api/auth/login',
-      allUsers: 'https://survey.vitensenteret.com:2000/api/auth/all_users',
-      delete: 'https://survey.vitensenteret.com:2000/api/auth/delete_account',
-      refer: 'https://survey.vitensenteret.com:2000/api/auth/get_referral_link/',
-      renewJWT: 'https://survey.vitensenteret.com:2000/api/auth/get_token/',
-      newEmail: 'https://survey.vitensenteret.com:2000/api/auth/change_email/',
-      newPassword: 'https://survey.vitensenteret.com:2000/api/auth/change_password/',
-      survey: 'https://survey.vitensenteret.com:2000/api/survey',
-      surveyAsJson: 'https://survey.vitensenteret.com:2000/api/survey/json',
-      surveyAsCSV: 'https://survey.vitensenteret.com:2000/api/survey/csv',
-      newUser: 'https://survey.vitensenteret.com:2000/api/auth/register',
-      checkChoosesurvey: 'https://survey.vitensenteret.com:2000/api/survey/escape',
-      surveyCopy:   'https://survey.vitensenteret.com:2000/api/survey/copy',
-      allNicknames: 'https://survey.vitensenteret.com:2000/api/survey/all_nicknames',
+      login: 'https://survey.vitensenteret.com/api/auth/login',
+      allUsers: 'https://survey.vitensenteret.com/api/auth/all_users',
+      delete: 'https://survey.vitensenteret.com/api/auth/delete_account',
+      refer: 'https://survey.vitensenteret.com/api/auth/get_referral_link/',
+      renewJWT: 'https://survey.vitensenteret.com/api/auth/get_token/',
+      newEmail: 'https://survey.vitensenteret.com/api/auth/change_email/',
+      newPassword: 'https://survey.vitensenteret.com/api/auth/change_password/',
+      survey: 'https://survey.vitensenteret.com/api/survey',
+      surveyAsJson: 'https://survey.vitensenteret.com/api/survey/json',
+      surveyAsCSV: 'https://survey.vitensenteret.com/api/survey/csv',
+      newUser: 'https://survey.vitensenteret.com/api/auth/register',
+      checkChoosesurvey: 'https://survey.vitensenteret.com/api/survey/escape',
+      surveyCopy:   'https://survey.vitensenteret.com/api/survey/copy',
+      allNicknames: 'https://survey.vitensenteret.com/api/survey/all_nicknames',
     }
 };
